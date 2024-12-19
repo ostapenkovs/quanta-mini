f9f44b65d0a58dd2551fe4ef082e6db06735619d
Vasiliy Ostapenko <ostapenkovs@pm.me>
2024-12-19 10:55:02 -0800

    bugfixing

--- app/app.py
+++ app/app.py
@@ -7,22 +7,27 @@ import wrds
 from flask import (
     Flask,
     request,
-    session,
     render_template,
     abort,
-    send_file,
+    redirect,
     url_for,
-    redirect
+    send_file,
+    send_from_directory
 )
 
 from util import get_data
 
 N_DAYS = 20
-
-app = Flask(__name__)
+UPLOAD_FOLDER = './data'
+OUTPUT_NAME = 'output.csv'
 
 load_dotenv()
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 conn = None
 def get_conn():
@@ -41,54 +46,58 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    output = None
+
     try:
         if request.method == 'POST':
-            ticker = request.form.get('ticker', None)
+            ticker = request.form.get('ticker')
             ticker = ticker.upper()
 
-            start_date = request.form.get('start_date', None)
-            end_date = request.form.get('end_date', None)
-            volume_breakout_thresh = request.form.get('volume_breakout_thresh', None)
+            start_date = request.form.get('start_date')
+            end_date = request.form.get('end_date')
+            volume_breakout_thresh = request.form.get('volume_breakout_thresh')
             volume_breakout_thresh = float(volume_breakout_thresh) / 100
 
-            change_thresh = request.form.get('change_thresh', None)
+            change_thresh = request.form.get('change_thresh')
             change_thresh = float(change_thresh) / 100
 
-            holding_period = request.form.get('holding_period', None)
+            holding_period = request.form.get('holding_period')
             holding_period = int(holding_period)
 
             c = get_conn()
             df = get_data(conn=c, ticker=ticker, start_date=start_date, end_date=end_date)
 
+            ### SIGNAL ###
             df['signal'] = (
                 (df['volume'] > (1+volume_breakout_thresh) * df['volume'].rolling(window=N_DAYS).mean()) & \
                 (df['return'] > change_thresh)
             ).astype(int)
 
             df = df.iloc[N_DAYS-1:]
+            ### SIGNAL ###
 
+            ### STRATEGY ###
             output = list()
             for idx in df[df['signal'] == 1].index:
                 date = df.loc[idx, 'date']
                 ret = (1 + df.loc[idx+1: idx+holding_period, 'return']).cumprod().iloc[-1] - 1
                 output.append((date, ret))
-            session['output'] = pd.DataFrame(output, columns=['date', 'return']).to_json()
+            output = pd.DataFrame(output, columns=['date', 'return'])
+            output.to_csv(os.path.join(app.config['UPLOAD_FOLDER'], OUTPUT_NAME), index=False)
+            output = output.to_html()
+            ### STRATEGY ###
     
     except:
         traceback.print_exc()
         abort(500)
     
-    return render_template('index.jinja', output='output' in session)
+    return render_template('index.jinja', output=output)
 
 @app.route('/download', methods=['GET'])
 def download():
     try:
-        if 'output' in session:
-            csv_buffer = io.BytesIO()
-            pd.read_json(io.StringIO(session['output'])).to_csv(csv_buffer, index=False)
-            csv_buffer.seek(0)
-            
-            return send_file(csv_buffer, mimetype='text/csv', as_attachment=True, download_name='output.csv')
+        if os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], OUTPUT_NAME)):
+            return send_from_directory(directory=app.config['UPLOAD_FOLDER'], path=OUTPUT_NAME)
 
         return redirect(url_for('index'))
     
