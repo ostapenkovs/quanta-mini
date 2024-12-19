9fe1f52d2c92b1ea60498521fa3c7d3f5ddb8848
Vasiliy Ostapenko <ostapenkovs@pm.me>
2024-12-18 18:56:52 -0800

    sql

--- app/app.py
+++ app/app.py
@@ -1,22 +1,35 @@
 import traceback
-import io
+import os, io
+from dotenv import load_dotenv
 import pandas as pd
 import wrds
 
 from flask import (
     Flask,
     request,
+    session,
     render_template,
     abort,
     send_file,
-    make_response
+    url_for,
+    redirect
 )
 
+from util import get_data
+
 N_DAYS = 20
-output = None
 
 app = Flask(__name__)
-conn = wrds.Connection()
+
+load_dotenv()
+app.secret_key = os.getenv('SECRET_KEY')
+
+conn = None
+def get_conn():
+    global conn
+    if conn is None:
+        conn = wrds.Connection()
+    return conn
 
 @app.errorhandler(404)
 def internal_server_error(e):
@@ -28,12 +41,8 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global output
-
     try:
         if request.method == 'POST':
-            # print(request.form)
-
             ticker = request.form.get('ticker', None)
             ticker = ticker.upper()
 
@@ -48,15 +57,8 @@ def index():
             holding_period = request.form.get('holding_period', None)
             holding_period = int(holding_period)
 
-            df = conn.raw_sql(
-                f"select returns.date, returns.vol as volume, returns.ret as return \
-                from crsp.dsenames as meta, crsp.dsf as returns \
-                where returns.date >= '{start_date}' and returns.date < '{end_date}' \
-                and returns.date >= meta.namedt and returns.date < meta.nameendt \
-                and meta.ticker = '{ticker}' and meta.permno = returns.permno", date_cols=['date']
-            )
-
-            # print(df)
+            c = get_conn()
+            df = get_data(conn=c, ticker=ticker, start_date=start_date, end_date=end_date)
 
             df['signal'] = (
                 (df['volume'] > (1+volume_breakout_thresh) * df['volume'].rolling(window=N_DAYS).mean()) & \
@@ -70,26 +72,25 @@ def index():
                 date = df.loc[idx, 'date']
                 ret = (1 + df.loc[idx+1: idx+holding_period, 'return']).cumprod().iloc[-1] - 1
                 output.append((date, ret))
-            output = pd.DataFrame(output, columns=['date', 'return'])
-
-            # print(output)
+            session['output'] = pd.DataFrame(output, columns=['date', 'return']).to_json()
     
     except:
         traceback.print_exc()
         abort(500)
     
-    return render_template('index.jinja', output=output.to_html() if output is not None else None)
+    return render_template('index.jinja', output='output' in session)
 
 @app.route('/download', methods=['GET'])
 def download():
-    global output
-
     try:
-        csv_buffer = io.BytesIO()
-        output.to_csv(csv_buffer, index=False)
-        csv_buffer.seek(0)
-        
-        return send_file(csv_buffer, mimetype='text/csv', as_attachment=True, download_name='output.csv')
+        if 'output' in session:
+            csv_buffer = io.BytesIO()
+            pd.read_json(io.StringIO(session['output'])).to_csv(csv_buffer, index=False)
+            csv_buffer.seek(0)
+            
+            return send_file(csv_buffer, mimetype='text/csv', as_attachment=True, download_name='output.csv')
+
+        return redirect(url_for('index'))
     
     except:
         traceback.print_exc()
